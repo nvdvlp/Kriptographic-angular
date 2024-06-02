714a158b07f269c84722d2c34f1b30c656d08bfe
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-06-01 20:53:27 -0500

    segundo cambio

--- kripto-angular/src/app/components/about-us/about-us.component.ts
+++ kripto-angular/src/app/components/about-us/about-us.component.ts
@@ -39,10 +39,10 @@ export class AboutUsComponent implements AfterViewInit {
     this.divElement.nativeElement.appendChild(container);
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.set(0, 100, 300);
+    camera.position.set(0, 150, 300);
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xa0a0a0);
+    scene.background = new THREE.Color(0x09090E);
 
     const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
     hemiLight.position.set(0, 200, 0);
@@ -55,21 +55,37 @@ export class AboutUsComponent implements AfterViewInit {
     dirLight.shadow.camera.bottom = -100;
     dirLight.shadow.camera.left = -120;
     dirLight.shadow.camera.right = 120;
-    scene.add(dirLight);
 
-    scene.add(new THREE.CameraHelper(dirLight.shadow.camera));
-
+    scene.add(dirLight);	
+	
+    // scene.add(new THREE.CameraHelper(dirLight.shadow.camera));
+	
     // Ground
-    const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
-    mesh.rotation.x = -Math.PI / 2;
-    mesh.receiveShadow = true;
-    scene.add(mesh);
+    // const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
+    // mesh.rotation.x = -Math.PI / 2;
+    // mesh.receiveShadow = true;
+    // scene.add(mesh);
 
     // Model
     const loader = new FBXLoader();
     loader.load('../../assets/models/puffer.fbx', function (object: any) {
+		
+		const boundingBox = new THREE.Box3().setFromObject(object);
+		const size = new THREE.Vector3();
+		boundingBox.getSize(size);
+
+		// Configurar el tamaño del canvas
+		renderer.setSize(size.x, size.y);
+		console.log("Ancho del modelo:", size.x);
+		console.log("Alto del modelo:", size.y);
+
+		const aspectRatio = size.x / size.y;
+		console.log("Proporción del modelo:", aspectRatio);
+
       object.traverse(function (child: any) {
         if (child.isMesh) {
+		  child.material.flatShading = true; 
+          child.material.wireframe = true; 	
           child.castShadow = true;
           child.receiveShadow = true;
         }
@@ -82,16 +98,30 @@ export class AboutUsComponent implements AfterViewInit {
 	transformControls.attach(object);
 	transformControls.setMode('rotate');
 	scene.add(transformControls);
+	let rotatingManually = false;
 
-      transformControls.addEventListener('mousedown', function () {
-        mouseDownOnMesh = true;
-        rotating = false; // Detener rotación automática
-      });
+	transformControls.showX = false; 
+	transformControls.showY = false; 
+	transformControls.showZ = false; 
 
-      transformControls.addEventListener('mouseup', function () {
-        mouseDownOnMesh = false;
-        rotating = true; // Reanudar rotación automática
-      });
+	transformControls.addEventListener('mousedown', function () {
+		mouseDownOnMesh = true;
+		rotating = false; 
+		rotatingManually = true;
+	});
+	
+	transformControls.addEventListener('mouseup', function () {
+		mouseDownOnMesh = false;
+		rotating = !rotatingManually; 
+		rotatingManually = false; 
+	});
+	
+	renderer.domElement.addEventListener('mouseleave', function () {
+		if (rotatingManually) {
+			rotatingManually = false;
+			rotating = true;
+		}
+	});
 
       function animate() {
         requestAnimationFrame(animate);
@@ -116,9 +146,9 @@ export class AboutUsComponent implements AfterViewInit {
     renderer.shadowMap.enabled = true;
     container.appendChild(renderer.domElement);
 
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 100, 0);
-    controls.update();
+    // controls = new OrbitControls(camera, renderer.domElement);
+    // controls.target.set(0, 100, 0);
+    // controls.update();
 
     window.addEventListener('resize', onWindowResize);
 
