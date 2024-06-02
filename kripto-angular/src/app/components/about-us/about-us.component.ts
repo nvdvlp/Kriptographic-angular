830a013f22ab05f809cd13beda936422b9f50fe2
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-06-01 19:23:29 -0500

    jacket rotation

--- kripto-angular/src/app/components/about-us/about-us.component.ts
+++ kripto-angular/src/app/components/about-us/about-us.component.ts
@@ -3,10 +3,12 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import Stats from 'three/addons/libs/stats.module.js';
 //@ts-ignore
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// @ts-ignore
+//@ts-ignore
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-  import * as THREE from 'three';
+//@ts-ignore
+import * as THREE from 'three';
+//@ts-ignore
+import { TransformControls } from 'three/addons/controls/TransformControls.js';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,139 +16,121 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css',
-  providers:[
+  styleUrls: ['./about-us.component.css'],
+  providers: [
     { provide: Window, useValue: window }
   ]
 })
-
-// export class AboutUsComponent implements AfterViewInit {
-export class AboutUsComponent{
-  constructor(private window:Window){
-
-  }
+export class AboutUsComponent implements AfterViewInit {
+  constructor(private window: Window) { }
 
   @ViewChild('div') divElement!: ElementRef;
   @ViewChild('h1') h1Element!: ElementRef;
 
-  ngAfterViewInit(): void{
-    let camera: any, scene: any, renderer: any, stats: any;
+  ngAfterViewInit(): void {
+    let camera: any, scene: any, renderer: any, controls: any;
+    let transformControls: any;
+    let rotating = true;
+    let mouseDownOnMesh = false;
+    const localThis = this;
 
-	const clock = new THREE.Clock();
+    // Crear canvas y ponerlo dentro del div del about-us
+    const container = document.createElement('div');
+    this.divElement.nativeElement.appendChild(container);
 
-	let mixer: any;
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.set(0, 100, 300);
 
-	const localThis = this
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
 
-	//Crear canvas y ponerlo dentro del div del about-us
-    const container = document.createElement( 'div' );
-	this.divElement.nativeElement.appendChild( container );
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
+    hemiLight.position.set(0, 200, 0);
+    scene.add(hemiLight);
 
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.set( 0, 100, 300 );
+    const dirLight = new THREE.DirectionalLight(0xffffff, 5);
+    dirLight.position.set(0, 200, 100);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 180;
+    dirLight.shadow.camera.bottom = -100;
+    dirLight.shadow.camera.left = -120;
+    dirLight.shadow.camera.right = 120;
+    scene.add(dirLight);
 
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xa0a0a0 );
-	// scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+    scene.add(new THREE.CameraHelper(dirLight.shadow.camera));
 
-	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 5 );
-	hemiLight.position.set( 0, 200, 0 );
-	scene.add( hemiLight );
+    // Ground
+    const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.receiveShadow = true;
+    scene.add(mesh);
 
-	const dirLight = new THREE.DirectionalLight( 0xffffff, 5 );
-	dirLight.position.set( 0, 200, 100 );
-	dirLight.castShadow = true;
-	dirLight.shadow.camera.top = 180;
-	dirLight.shadow.camera.bottom = - 100;
-	dirLight.shadow.camera.left = - 120;
-	dirLight.shadow.camera.right = 120;
-	scene.add( dirLight );
-
-	scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
-
-	// ground
-	const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-	mesh.rotation.x = - Math.PI / 2;
-	mesh.receiveShadow = true;
-	scene.add( mesh );
-
-	// const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
-	// grid.material.opacity = 0.2;
-	// grid.material.transparent = true;
-	// scene.add( grid );
-
-				// model
-				const loader = new FBXLoader();
-				loader.load( '../../assets/models/puffer.fbx', function ( object:any ) {
-
-					// mixer = new THREE.AnimationMixer( object );
-
-					// const action = mixer.clipAction( object.animations[ 0 ] );
-					// action.play();
-
-					object.traverse( function ( child:any ) {
-						if ( child.isMesh ) {
-
-							child.castShadow = true;
-							child.receiveShadow = true;
-
-						}
-
-					} );
-
-					scene.add( object );
-					localThis.h1Element.nativeElement.style.display = 'none'
-					//alert("loaded")
-
-				} );
-
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.shadowMap.enabled = true;
-				container.appendChild( renderer.domElement );
-
-				const controls = new OrbitControls( camera, renderer.domElement );
-				controls.target.set( 0, 100, 0 );
-				controls.update();
-
-				window.addEventListener( 'resize', onWindowResize );
-
-				// stats
-				//stats = new Stats();
-				//container.appendChild( stats.dom );
-        animate()
-
-
-        function onWindowResize() {
-
-          camera.aspect = window.innerWidth / window.innerHeight;
-          camera.updateProjectionMatrix();
-  
-          renderer.setSize( window.innerWidth, window.innerHeight );
-		  render()
-  
+    // Model
+    const loader = new FBXLoader();
+    loader.load('../../assets/models/puffer.fbx', function (object: any) {
+      object.traverse(function (child: any) {
+        if (child.isMesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
         }
-  
-        
-  
-        function animate() {
-  
-          requestAnimationFrame( animate );
-  
-          //const delta = clock.getDelta();
-  
-          //if ( mixer ) mixer.update( delta );
+      });
+      scene.add(object);
+      localThis.h1Element.nativeElement.style.display = 'none';
 
-          render()
-  
-          //stats.update();
-  
+	// TransformControls
+	transformControls = new TransformControls(camera, renderer.domElement);
+	transformControls.attach(object);
+	transformControls.setMode('rotate');
+	scene.add(transformControls);
+
+      transformControls.addEventListener('mousedown', function () {
+        mouseDownOnMesh = true;
+        rotating = false; // Detener rotación automática
+      });
+
+      transformControls.addEventListener('mouseup', function () {
+        mouseDownOnMesh = false;
+        rotating = true; // Reanudar rotación automática
+      });
+
+      function animate() {
+        requestAnimationFrame(animate);
+
+        if (rotating && !mouseDownOnMesh) {
+          object.rotation.y += 0.009;
         }
 
-		function render(){
-			renderer.render( scene, camera );
-		}
+        render();
+      }
+
+      function render() {
+        renderer.render(scene, camera);
+      }
+
+      animate();
+    });
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    container.appendChild(renderer.domElement);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 100, 0);
+    controls.update();
+
+    window.addEventListener('resize', onWindowResize);
+
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      render();
+    }
+
+    function render() {
+      renderer.render(scene, camera);
+    }
   }
-
 }
