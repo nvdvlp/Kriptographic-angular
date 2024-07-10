3553d5fcdf43dcaf64c85bca3925f4e71bea4e00
David Barón <baronavilandavid@gmail.com>
2024-07-10 08:58:32 -0500

    Se implementa posicionamiento condicional de las cartas

--- kripto-angular/src/app/app.component.ts
+++ kripto-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; 
@@ -7,6 +7,7 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HamMenuComponent } from './components/ham-menu/ham-menu.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
@@ -19,11 +20,47 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
     ContactUsComponent, 
     FormsModule,
     HamMenuComponent,
-    ServicesComponent
+    ServicesComponent,
+    LoaderComponent
   ] ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'kripto-angular';
+export class AppComponent implements OnInit {
+  title = 'Kriptographic';
+  showLoader: boolean = true;
+  fontLoaded: boolean = false;
+  gifLoaded: boolean = false;
+
+  ngOnInit(): void {
+    const font = new FontFace('Medium', 'url(assets/fonts/Medium/medium.otf)');
+    const thisa = this;
+
+    const interval = setInterval((): any => {
+      if(this.fontLoaded && this.gifLoaded){
+        this.showLoader = false;
+        console.log("closed interval")
+        clearInterval(interval)
+      }
+    }, 0o5)
+    // Load the font
+    font.load().then(function(loadedFont) {
+      // @ts-ignore
+      document.fonts.add(loadedFont);
+    
+      thisa.fontLoaded = true
+      if(thisa.fontLoaded && thisa.gifLoaded){
+        thisa.showLoader = false;
+      }
+    }).catch(function(error) {
+      console.error('Font failed to load:', error);
+    });
+  }
+
+  checkLoads(){
+    this.gifLoaded = true;
+    if(this.fontLoaded && this.gifLoaded){
+      this.showLoader = false;
+    }
+  }
 }
--- kripto-angular/src/app/components/services/services.component.ts
+++ kripto-angular/src/app/components/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import {FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -10,9 +10,42 @@ import { CommonModule } from '@angular/common';
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
-export class ServicesComponent{
+export class ServicesComponent implements AfterViewInit{
   animacionActiva: boolean = false;
   tarjetaActiva: number = 1;
+
+  cardArray: any[] = [
+    {
+      title: 'GAMER INSIGHTS',
+      content: 'Harness the power of consumer behavior data to tailor gaming experiences to player preferences.This data-driven approach informs strategic decisions, ensuring that branded content resonates with gamers, drives engagement, and cultivates lasting brand affinity.'
+    },
+    {
+      title: 'AI 3D ASSET CREATION',
+      content: 'Transform brands into digital assets with unparalleled realism and detail. Leveraging cutting-edge AI technology, we meticulously craft 3D models of branded apparel and accessories. These assets replicate the aesthetic appeal of real-world products delivering lifelike visuals and enhancing player immersion.'
+    },
+    {
+      title: 'LICENSING NEGOTIATION',
+      content: 'Navigate the complexities of brand licensing with confidence, ensuring access to coveted intellectual property. From iconic fashion houses to trendsetting labels, we secure rights that enhance authenticity and enrich gameplay, empowering studios to create immersive virtual worlds.'
+    },
+  ]
+
+  ngAfterViewInit() {
+    const cardsNodeList = document.querySelectorAll('.services__showCard');
+    let startingPosition = 20;
+    let zIndexStartingPosition = cardsNodeList.length;
+    cardsNodeList.forEach((card: any, index) => {
+      card.style.zIndex = zIndexStartingPosition
+      if(index > 0){
+        card.style.top = `${startingPosition}px`
+        startingPosition += 20
+      } else{
+        card.style.top = `0px`
+      }
+      zIndexStartingPosition -= 1;
+    })
+    //@ts-ignore
+    document.querySelector('.services__cards').style.minHeight = `${425 + (20 * cardsNodeList.length)}px`
+  }
 
   iniciarAnimacion() {
     this.animacionActiva = true;
