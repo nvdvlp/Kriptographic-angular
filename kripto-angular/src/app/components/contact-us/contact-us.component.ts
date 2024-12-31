fc8862e1ddeee230facc5f8f4ec190ad718d2eb0
David Barón <baronavilandavid@gmail.com>
2024-12-31 04:54:59 -0500

    final fixes

--- kripto-angular/src/app/components/contact-us/contact-us.component.ts
+++ kripto-angular/src/app/components/contact-us/contact-us.component.ts
@@ -1,3 +1,4 @@
+import { createClient } from '@supabase/supabase-js';
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -37,7 +38,10 @@ export class ContactUsComponent {
     userName: "",
     userEmail: "",
   })  
-   
+  
+  supabaseUrl: string = 'https://bcmpbgwpclcuojfmarpg.supabase.co';
+  supabaseKey: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbXBiZ3dwY2xjdW9qZm1hcnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4MTgxOTcsImV4cCI6MjA0NzM5NDE5N30.lFnvAPkNj8eLO3rpGGNrEK1qXJoYqkCXv6Qn4N73388';
+  supabase: any;
   // async send(){
   //   console.log("a")
   //   //USER ID santiago
@@ -54,6 +58,7 @@ export class ContactUsComponent {
   // }
 
   ngOnInit(){
+    this.supabase = createClient(this.supabaseUrl, this.supabaseKey);
     this.userForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       name: [''],
@@ -64,5 +69,26 @@ export class ContactUsComponent {
 
   onSubmit() {
     console.log('Form Submitted', this.userForm.value);
+    this.addEmail(this.userForm.value.email,this.userForm.value.name);
   }
+
+
+// Initialize the Supabase client
+
+  async addEmail(email: string, name: string) {
+  const { data, error } = await this.supabase
+    .from('Emails') // Replace with your actual table name
+    .insert([
+      { email, name } // Replace with any other fields you want to populate
+    ]);
+
+  if (error) {
+    console.error('Error inserting data:', error);
+  } else {
+    console.log('Data inserted successfully:', data);
+  }
+}
+
+// Example usage
+
 }
